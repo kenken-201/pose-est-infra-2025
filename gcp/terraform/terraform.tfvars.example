d0ac4797501219202575dc55d157fd736249ec81
kensak222 <66297692+kensak222@users.noreply.github.com>
2026-01-01 17:09:48 +0900

    update: リファクタリングを実施

---
+++ gcp/terraform/terraform.tfvars.example
@@ -0,0 +1,3 @@
+project_id  = "kenken-pose-est"
+region      = "asia-northeast1"
+environment = "dev"
--- gcp/terraform/versions.tf
+++ gcp/terraform/versions.tf
@@ -18,3 +18,13 @@ terraform {
     }
   }
 }
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+provider "google-beta" {
+  project = var.project_id
+  region  = var.region
+}
