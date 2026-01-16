503b3b672e435fd8227198564b7d6eb60d5f9688
佐々木絢悟 <p4k54z2o@s.okayama-u.ac.jp>
2026-01-16 17:06:43 +0900

    fix: use bindings attribute for cloudflare_workers_script

--- cloudflare/terraform/environments/dev/main.tf
+++ cloudflare/terraform/environments/dev/main.tf
@@ -71,7 +71,12 @@ resource "cloudflare_workers_script" "api_proxy_dev" {
   account_id  = var.cloudflare_account_id
   script_name = "pose-est-api-proxy-dev"
   
-  # Secret Binding は別リソース (cloudflare_workers_secret) で定義
+  # Secret Binding (認証トークン)
+  bindings = [{
+    name = "BACKEND_ACCESS_TOKEN"
+    type = "secret_text"
+    text = var.backend_access_token
+  }]
 
   # Worker Script 定義 (Inline)
   # 1. すべてのリクエスト ('fetch' event) を捕捉
@@ -156,12 +161,3 @@ module "monitoring" {
   zone_id    = var.cloudflare_zone_id
 }
 
-# -----------------------------------------------------------------------------
-# Workers Secret (Backend Access Token)
-# -----------------------------------------------------------------------------
-resource "cloudflare_workers_secret" "backend_access_token" {
-  account_id  = var.cloudflare_account_id
-  script_name = cloudflare_workers_script.api_proxy_dev.script_name
-  name        = "BACKEND_ACCESS_TOKEN"
-  secret_text = var.backend_access_token
-}
